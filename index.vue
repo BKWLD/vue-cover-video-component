cfd302b5a34b95dbc8b86ead99802e6a80f2f27c
Robert Reinhard <robert.reinhard@bkwld.com>
2016-03-01 17:23:30 -0800

    Simplifying and cleaning
    And adding finer controls over autoplay/pause

--- index.vue
+++ index.vue
@@ -3,7 +3,7 @@ A fullscren video player that simulates background-cover for video
 -->
 
 <template lang='jade'>
-.cover-video(:class='class')
+.cover-video(:class='[trim, playable ? "playable" : ""]')
 
 	//- Poster image loaded dynamically
 	.bkgd(v-el:poster v-if="poster"
@@ -11,8 +11,7 @@ A fullscren video player that simulates background-cover for video
 		:media="poster")
 
 	//- Container for the <video> element
-	.video(v-el:video
-		:style='{opacity: 1}')
+	.video(v-el:video)
 
 </template>
 
@@ -26,19 +25,47 @@ module.exports =
 
 	# Settings
 	props:
-		autoplay: default: 'now'
-		loop:     default: true
-		mute:     default: true
-		video:    defaut: null
-		poster:   defaut: null
+
+		# Video HTML string
+		video: default: null
+
+		# Control the loading of video
+		autoload:
+			default: 'scroll'
+			validator: (value) -> value in [
+				'scroll' # Load the video when it is scrolled into viewport
+				true     # Load the video immediately
+				false    # Don't autoload
+			]
+
+		# Control the playing of video
+		autoplay:
+			default: 'scroll'
+			validator: (value) -> value in [
+				'scroll' # Play the video whnever it is scrolled into viewport
+				true     # Play immediately, as soon as it is loaded
+				false    # Don't autoplay
+			]
+
+		# Control the pausing of video
+		autopause:
+			default: 'scroll'
+			validator: (value) -> value in [
+				'scroll' # Pause the video whnever it is scrolled out of viewport
+				false    # Don't do any auto pausing control
+			]
+
+		# Simple HTML5 video properties
+		loop:      default: true
+		mute:      default: true
+		poster:    defaut: null
 
 	data: ->
-		vid:       null
-		class:     null
-		aspect:    null
-		landscape: null
-		visible:   null
-		playable:  false
+		vid:             null   # The video element
+		playable:        false  # Is the video ready to play?
+		videoAspect:     null   # Aspect ratio of the video
+		containerAspect: null   # Orientation of the container
+		class:           null   # Cropping class to apply
 
 	# We want to autopause using in-viewport
 	created: -> @inViewportOnce = false
@@ -53,105 +80,92 @@ module.exports =
 
 		# Init HTML5 video, which may be absent during dev
 		initVideo: ->
-
-			# Wait till it's scrolled into visiblity to load and play
-			if @autoplay == 'scroll'
-				unwatch = @$watch 'inViewport', (visible) ->
-					return unless visible
-					unwatch()
-					@load()
-					@playIfVisible()
-				, immediate: true
-
-			# Start loading (and possibly playing) now
-			else
+			if @autoload == 'scroll'
+				@loadWhenVisible()
+			else if @autoload || @autoplay
 				@load()
-				@playIfVisible()
 
-		# Start the video loading
+		# Start loading once the video is in the viewport
+		loadWhenVisible: ->
+			unwatch = @$watch 'inViewport', (visible) ->
+				return unless visible
+				unwatch()
+				@load()
+			, immediate: true
+
+		# Start the video loading.  Watching for it via `timeupdate` because it
+		# reports playback earlier and more dependably than `canplaythrough`,
+		# noticeably in Safari.  The video begins loading when the video is appended
+		# because it is set to repload.
 		load: ->
+			@vid = @renderVideo(@video)
+			@vid.addEventListener 'loadedmetadata', @onAspectData
+			@vid.addEventListener 'timeupdate', @canPlay
+			@$els.video.appendChild @vid
+			@play() if @autoplay == true
+			if @autoplay == 'scroll' || @autopause == 'scroll'
+				@$watch 'inViewport', @toggleIfVisible, immediate: true
 
-			# Make the element
-			@vid = @videoEl
+		# Take video tag HTML string and render DOM element
+		renderVideo: (html) ->
+			$html = $(@video)
+			$html.attr 'loop', true if @loop
+			$html.attr 'autoplay', true if @autoplay == 'now'
+			$html.attr 'muted', true if @mute
+			$html.attr 'preload', 'auto'
+			return $html[0]
 
-			# Listen for resizing
-			win.on 'resize', _.bind(@onResize, @)
+		# The video is ready for playing.  Doing a timeupdate check because
+		# canplaythrough was firing too early and would pause for a tick in safari.
+		canPlay: (e) ->
+
+			# If autoplaying, make sure it's already advanced before showing
+			return if @vid.currentTime < 0.2
+			@vid.removeEventListener 'timeupdate', @canPlay
+
+			# Listen to resizing to capture
+			win.on 'resize', @onResize
 			@onResize()
 
-			# Listen for scrolling
-			@$watch 'inViewport', @playIfVisible
+			# Recapture the aspect if it failed the first time.  Safari is guilty
+			# of this.
+			@captureAspect() if !@aspect
 
-			# Listen for being able to play differntly based on what kinda autoplay
-			if @autoplay == 'now'
-				@vid.addEventListener 'timeupdate', @canPlayVideo
-			else
-				@vid.addEventListener 'canplaythrough', @canPlayVideo
-
-			# Listen for when the aspect ratio can be set
-			@vid.addEventListener 'loadedmetadata', @onAspectData
-
-			# Insert the html via JS when ready to play.  Using the v-html directive to
-			# set the video content was triggering multiple video loads.
-			# loader.load()
-			@$els.video.appendChild @vid
+			# Shows the video
+			@playable = true
 
 		# The video has reported it is playable
 		onAspectData: (e) ->
 			@vid.removeEventListener 'loadedmetadata', @onAspectData
 			@captureAspect()
 
-		# Remove the video
-		destroyVideo: ->
-			win.off 'resize', _.bind(@onResize, @)
-			@vid.removeEventListener 'loadedmetadata', @onAspectData
-			@vid.pause()
-			@vid.src = ''
-			@vid.load()
-
 		# Capture the aspect ratio
 		captureAspect: (e) ->
-			@aspect = @vid.videoWidth / @vid.videoHeight
+			@videoAspect = @vid.videoWidth / @vid.videoHeight
 			@onResize()
 
 		# Center the video in it's container while maintaining aspect ratio
 		onResize: (e) ->
-
-			# If we're mid remove, don't fire
 			return unless @$el
+			@containerAspect = @$el.clientWidth / @$el.clientHeight
 
-			# Store the orientation
-			ratio = @$el.clientWidth / @$el.clientHeight
-			@landscape = ratio > 1
+		# Remove the video
+		destroyVideo: ->
+			win.off 'resize', @onResize
+			@vid.removeEventListener 'loadedmetadata', @onAspectData
+			@vid.removeEventListener 'timeupdate', @canPlay
+			@vid.pause()
+			@vid.src = ''
+			@vid.load()
 
-			# Store how to crop it
-			if @aspect and ratio > @aspect
-				@class = 'trim-vertical'
-			else
-				@class = 'trim-horizontal'
-
-		# The video is ready for playing.  Doing a timeupdate check because
-		# canplaythrough was firing too early and would pause for a tick in safari.
-		canPlayVideo: (e) ->
-
-			# If autoplaying, make sure it's already advanced before showing
-			if @autoplay == 'now'
-				return if @vid.currentTime < 0.2
-				$(@$els.poster).velocity { opacity: 0 },{ display: 'none', duration: 100 }
-				@vid.removeEventListener 'timeupdate', @canPlayVideo
-
-			# If not autoplaying, trust the canplaythrough
-			else
-				@vid.addEventListener 'canplaythrough', @canPlayVideo
-
-			# Recapture the aspect if it failed the first time.  Safari is guilty
-			# of this.
-			@captureAspect() if !@aspect
-
-			# Show video
-			@playable = true
+		###
+		# Controlling video API
+		###
 
 		# Play the video if visible
-		playIfVisible: (val) -> @toggle(@inViewport)
+		toggleIfVisible: (val) ->
+			@play() if @inViewport && @autoplay == 'scroll'
+			@pause() if not @inViewport && @autopause == 'scroll'
 
 		# Toggle video playing state
 		toggle: (play) -> if play then @play() else @pause()
@@ -170,15 +184,9 @@ module.exports =
 		pause: -> @vid.pause()
 
 	computed:
-		# Customize the video HTML tag
-		videoEl: ->
-			$html = $(@video)
-			$html.attr 'loop', true if @loop
-			$html.attr 'autoplay', true if @autoplay == 'now'
-			$html.attr 'muted', true if @mute
 
-			$html.attr 'preload', 'auto'
-			$html[0]
+		# The aspect ratio of the video as a string
+		trim: ->if @containerAspect > @videoAspect then 'letterbox' else 'pillarbox'
 
 </script>
 
@@ -198,6 +206,10 @@ module.exports =
 		left 0
 		bottom 0
 		right 0
+		opacity 0
+		transition opacity 300ms
+	&.playable .video
+		opacity 1
 
 	video
 		position absolute
@@ -205,13 +217,11 @@ module.exports =
 		left 50%
 		transform translate(-50%, -50%)
 
-	&.trim-horizontal video
-		// Default is to pillarbox
+	&.pillarbox video
 		width auto
 		height calc(100% + 1px) // Cover rounding errors
 
-	// Letterbox
-	&.trim-vertical video
+	&.letterbox video
 		width calc(100% + 1px) // Cover rounding errors
 		height auto;
 
